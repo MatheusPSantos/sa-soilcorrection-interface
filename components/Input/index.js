09386768abd90bb2a71e7c055821174c9040e609
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-10-28 00:19:03 -0300

    atualizando componente inpur

--- components/Input/index.js
+++ components/Input/index.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 export const Input = styled.input`
-    width: auto;
     height: 30px;
     margin: 0px 5px;
+
+    &:hover {
+        border: blue 1px solid;
+    }
 `;
 
 export const InputField = styled.span`
     margin: 5px 2px;
+    display: flex;
+    flex-direction: column;
 `;
